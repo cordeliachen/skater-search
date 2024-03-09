a0b9ccecabfd4500651679dce941f5feee68f5e2
wahooey <cc4655@columbia.edu>
2024-03-08 23:08:22 -0500

    yippee

--- static/search.js
+++ static/search.js
@@ -7,4 +7,13 @@ $(document).ready(function () {
       $("#search-input").val("");
     }
   });
+
+  $(".word-search").click(function () {
+    // Get the text of the clicked word
+    var searchText = $(this).text().trim();
+    // Set the search input value to the clicked word
+    $("#search-input").val(searchText);
+    // Submit the form
+    $("#search-form").submit();
+  });
 });
